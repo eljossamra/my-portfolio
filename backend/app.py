94142f45d4bd9f76521e0004f0a7ab75d2f5faa4
Eljos <samaraeljos@gmail.com>
2025-04-13 02:57:17 -0400

    Bind port to fix deployment

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,4 +11,5 @@ def hello():
 
 if __name__ == "__main__":
     print("Starting Flask app...")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
